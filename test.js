849d6570c6754f81462ae35d3b766e0b1544dc0c
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 21:16:18 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-'use strict';
-var test = require('ava');
-var reactDomPragma = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
-	var a = '/** @jsx React.DOM */\nvar foo = true;';
-	t.assert(reactDomPragma(a) === a);
-	t.assert(reactDomPragma('var foo = true;') === a);
-	t.end();
+test(t => {
+	const a = '/** @jsx React.DOM */\nvar foo = true;';
+	t.is(fn(a), a);
+	t.is(fn('var foo = true;'), a);
 });
